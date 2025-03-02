5d669eed4ce08c9cef4c347e548a66c1a3a39801
Rodolfo Noguez <rodolfonoguez10@gmail.com>
2025-03-01 18:48:36 -0600

    v1 if the ui/not done

--- popup/popup.js
+++ popup/popup.js
@@ -1,1 +1,33 @@
-//TODO
+// TODO
+const toggleButton = document.querySelector(".theme-toggle");
+
+function themetoggle(){
+    document.body.classList.toggle("light-mode");
+    toggleButton.classList.toggle("button-light-mode");
+
+    if (document.body.classList.contains("light-mode")){
+        localStorage.setItem("theme", "light");
+    }
+    else{
+        localStorage.setItem("theme", "dark");
+    }
+
+}
+
+toggleButton.addEventListener("click", themetoggle);
+
+
+if(localStorage.getItem("theme")==="light"){
+    document.body.classList.add("light-mode");
+    toggleButton.classList.add("button-light-mode")
+}
+
+
+// function myFunction() {
+//     var element = document.body;
+//     element.classList.toggle("light-mode");
+//  }
+
+//  const toggleButton = document.querySelector(".theme-toggle");
+
+//  toggleButton.addEventListener("click", myFunction);
