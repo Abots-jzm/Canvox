8e9d9d9c0a80aabb62019dbb278127033f7271bd
Rodolfo Noguez <rodolfonoguez10@gmail.com>
2025-03-03 21:35:46 -0600

    new buttons added/need functionality

--- popup/popup.js
+++ popup/popup.js
@@ -1,9 +1,13 @@
 // TODO
 const toggleButton = document.querySelector(".theme-toggle");
+const transcriptButton = document.querySelector(".transcript");
+const hotkeyButton = document.querySelector(".hotkeys");
 
 function themetoggle(){
     document.body.classList.toggle("light-mode");
     toggleButton.classList.toggle("button-light-mode");
+    transcriptButton.classList.toggle("button-light-mode");
+    hotkeyButton.classList.toggle("button-light-mode");
 
     if (document.body.classList.contains("light-mode")){
         localStorage.setItem("theme", "light");
@@ -19,15 +23,7 @@ toggleButton.addEventListener("click", themetoggle);
 
 if(localStorage.getItem("theme")==="light"){
     document.body.classList.add("light-mode");
-    toggleButton.classList.add("button-light-mode")
+    toggleButton.classList.add("button-light-mode");
+    transcriptButton.classList.add('button-light-mode');
+    hotkeyButton.classList.add("button-light-mode");
 }
-
-
-// function myFunction() {
-//     var element = document.body;
-//     element.classList.toggle("light-mode");
-//  }
-
-//  const toggleButton = document.querySelector(".theme-toggle");
-
-//  toggleButton.addEventListener("click", myFunction);
